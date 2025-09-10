682dd5ae85f327aa3f4dde9f6330cffac174dffc
2300030813 <dedeepyaseelam02@gmail.com>
2025-09-10 23:22:54 +0530

    Updated config.js

--- FRONTEND/PATIENTAPI-REACT/src/config.js
+++ FRONTEND/PATIENTAPI-REACT/src/config.js
@@ -1,6 +1,6 @@
 // Backend API URL
 const config = {
-  API_URL: "http://localhost:2010/patientapi" // match Spring Boot port
+  API_URL: "http://localhost:2050/patientapi" // match Spring Boot port
 };
 
 export default config;
