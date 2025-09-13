583451c9043fa49e8a4e46b3d614a23e57365c7d
2300030813 <dedeepyaseelam02@gmail.com>
2025-09-13 12:15:46 +0530

    Updated PatientapiSpringbootApplication.java

--- BACKEND/PATIENTAPI-SPRINGBOOT/src/main/java/com/klef/PatientapiSpringbootApplication.java
+++ BACKEND/PATIENTAPI-SPRINGBOOT/src/main/java/com/klef/PatientapiSpringbootApplication.java
@@ -2,17 +2,20 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PatientapiSpringbootApplication extends SpringBootServletInitializer
-{
-	
+public class PatientapiSpringbootApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // tell Tomcat what the source is
+        return application.sources(PatientapiSpringbootApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatientapiSpringbootApplication.class, args);
-		System.out.println("Project is Running!");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PatientapiSpringbootApplication.class, args);
+        System.out.println("Project is Running!");
+    }
 }
