c55902208c6a04107b8dfb68267e25fb1a92ee4b
2300030813 <dedeepyaseelam02@gmail.com>
2025-09-14 11:37:17 +0530

    Updated config.js

--- FRONTEND/PATIENTAPI-REACT/src/config.js
+++ FRONTEND/PATIENTAPI-REACT/src/config.js
@@ -1,11 +1,7 @@
 const config = {
-  DEV_API_URL: "http://localhost:2010/patientapi",
-  TOMCAT_API_URL: "http://localhost:2030/springbootpatientapi/patientapi"
+  //API_URL: "http://localhost:2010/patientapi" 
+  // OR
+  API_URL: "http://localhost:2030/springbootpatientapi/patientapi"
 };
 
-const API_URL =
-  window.location.hostname === "localhost" && window.location.port === "5173"
-    ? config.DEV_API_URL
-    : config.TOMCAT_API_URL;
-
-export default API_URL;
+export default config;
